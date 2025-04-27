4b5be3c0533358ccff6ae9bd2260c23ef0ae9ff5
abubakr2146 <124148219+abubakr2146@users.noreply.github.com>
2025-04-27 17:19:41 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       className={cn(inter.variable, geistMono.variable, 'antialiased')}
     >
       <body className='bg-bg-white-0 text-text-strong-950'>
-        <ThemeProvider attribute='class'>
+        <ThemeProvider attribute='class' defaultTheme='light'>
           <TooltipProvider>
             <div className='flex min-h-screen flex-col'>
               <Navbar />
