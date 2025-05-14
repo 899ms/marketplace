d48377b67ba2cc874695ca8fa3d344f5c5f8e421
Ahmad-Aslam-exitech <ahmad.aslam@exitech.org>
2025-05-14 21:12:38 +0500

    bookmark

--- components/projects/detail/ProjectHeader.tsx
+++ components/projects/detail/ProjectHeader.tsx
@@ -4,6 +4,13 @@ import React from 'react';
 import * as Badge from '@/components/ui/badge';
 import * as Button from '@/components/ui/button';
 
+// Bookmark SVG Component
+const BookmarkIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13.8978 2H6.1122C4.40098 2 3 3.38917 3 5.08596V16.3582C3 17.797 4.04074 18.4122 5.31164 17.7077L9.24443 15.5346C9.66473 15.3064 10.3452 15.3064 10.7555 15.5346L14.6883 17.7077C15.9592 18.4122 16.9999 17.797 16.9999 16.3582V5.08596C17.01 3.38917 15.609 2 13.8978 2Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
+
 interface ProjectHeaderProps {
   title: string;
   category: string;
@@ -47,7 +54,7 @@ const ProjectHeader: React.FC<ProjectHeaderProps> = ({ title, category, showBook
             className="!text-gray-400 hover:!text-gray-500"
           >
             <Button.Icon>
-              <img src="/assets/images/icons/bookmark.svg" alt="Bookmark" className="size-5" />
+              <BookmarkIcon />
             </Button.Icon>
           </Button.Root>
         )}
