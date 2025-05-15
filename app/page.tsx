5ef354e73b5707944d9d3af73df1761b830f4e02
abubakr2146 <124148219+abubakr2146@users.noreply.github.com>
2025-05-15 19:19:31 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,10 @@ export default function RootPage() {
 
   return (
 
+
+
+    
+
     <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
       <Suspense fallback={<LoginFormSkeleton />}>
 
