16f62ec706e9fae4b3e27b260a24366b266a3a5f
Ahmad-Aslam-exitech <ahmad.aslam@exitech.org>
2025-05-23 21:00:11 +0500

    key back

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -7,7 +7,7 @@ export async function createSupabaseServerClient() {
   // Create a server-side client for Supabase. See https://supabase.com/docs/guides/getting-started/tutorials/with-nextjs
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         async get(name: string) {
